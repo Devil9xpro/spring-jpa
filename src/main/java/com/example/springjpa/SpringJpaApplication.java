66cb358cc9581c9eb3ad3394defb0baec010b546
Devil9xpro <chinhzz3107@gmail.com>
2021-05-16 20:04:46 +0700

    dont jdbc crud

---
+++ src/main/java/com/example/springjpa/SpringJpaApplication.java
@@ -0,0 +1,34 @@
+package com.example.springjpa;
+
+import com.example.springjpa.entities.Person;
+import com.example.springjpa.jdbc.PersonJdbcDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+
+@SpringBootApplication
+public class SpringJpaApplication implements CommandLineRunner {
+    @Autowired
+    private PersonJdbcDAO dao;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("All Users -> {}", dao.findAll());
+//        logger.info("User id 10001 -> {}", dao.findById(10001));
+//        logger.info("Delete 10002 -> No of rows deleted - {}", dao.deleteById(10002));
+//        logger.info("Inserting 10004 -> {}",dao.insert(new Person(10004,"Tara", "Berlin", new Date())));
+//        logger.info("Updating 10003 -> {}",dao.update(new Person(10003,"Danh", "Vietnam", new Date())));
+    }
+}
